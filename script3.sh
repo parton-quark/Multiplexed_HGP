6e0d6cfc4d77870414ade564119d99dadd3b01a3
parton-quark <parton@nii.ac.jp>
2024-03-14 23:51:53 +0900

    add scripts

---
+++ script3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p compute
+#SBATCH --time=15:00:00
+#SBATCH --mem=10G
+#SBATCH --job-name=multiplexing      # Job name\
+#SBATCH -c 1                    # Run all processes on a single node\
+#SBATCH --ntasks=1                   # Run a single task\
+#SBATCH --output=multiplexing_output_test.txt  # Standard output and error log\
+
+python multiplexing_VH_decoder.py 2 1 0 10000 0.55 0.00 55 16 2 4 1
