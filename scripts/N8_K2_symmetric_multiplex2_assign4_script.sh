68b65fa7a9e5cc9c675e632661ccbde9454c52c6
Nicholas-Connolly <39500615+Nicholas-Connolly@users.noreply.github.com>
2024-04-23 16:40:29 +0900

    Experiment 2 for HGP script

--- scripts/N8_K2_symmetric_multiplex2_assign4_script.sh
+++ scripts/N8_K2_symmetric_multiplex2_assign4_script.sh
@@ -7,6 +7,6 @@
 #SBATCH --job-name=2_100000_1_0_101_4_N8_K2_symmetric
 #SBATCH --output=output_2_100000_1_0_101_4_N8_K2_symmetric.txt
 
-module load python/3.11.4
 source /apps/unit/NemotoU/NicholasSoftware/bin/activate
+module load python/3.11.4
 python ../multiplexing_VH_decoder.py 2 100000 1 0 101 4 ../input_matrices/N8_K2_symmetric
