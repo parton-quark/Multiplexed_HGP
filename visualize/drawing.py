1cbe1f88272fc86a66fbe1fdb7891f3b7dd9e79a
Nicholas-Connolly <39500615+Nicholas-Connolly@users.noreply.github.com>
2024-05-02 15:21:01 +0900

    fix success/failure rate in plotting function

--- visualize/drawing.py
+++ visualize/drawing.py
@@ -25,26 +25,23 @@ def json_to_data(json_file_name):
     data = open(json_file_name)
     data = json.load(data)
     erasure_rates = data['erasure_rates']
-    success_rates = data['success_rate']
-    error_bar_for_success_rates = data['error_bar_for_success_rate']  
+    failure_rates = data['failure_rates']
+    failure_rates_error_bars = data['failure_rates_error_bars']  
     decoding_failure_rates = data['DFrate']
     non_df_le_rates = data['nonDFLErate']
 
-
     label = 'm=' + str(data['num_multiplexing']) + ' , strategy=' + str(data['assignment type'])
-    return label, erasure_rates, success_rates,error_bar_for_success_rates,decoding_failure_rates, non_df_le_rates
-
-
+    return label, erasure_rates, failure_rates,failure_rates_error_bars,decoding_failure_rates, non_df_le_rates
 
 def plot_multiple_data(list_of_res, phys_err, save = False, grid = True, log = False, with_le_and_df=False):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     
     for i in list_of_res:
-        ax.errorbar(phys_err, i[2], i[3], linewidth= 1, label = i[0])
+        ax.errorbar(phys_err, i[2], i[3], linewidth= 2, label = i[0])
         if with_le_and_df == True:
-            ax.plot(phys_err, i[4], linewidth = 1, label = i[0] + 'DF') # decoding failure rates
-            ax.plot(phys_err, i[5], linewidth = 1, label = i[0] + 'non DF LE') # non decoding failure logical error rates 
+            ax.plot(phys_err, i[4], linewidth = 2, label = i[0] + 'DF') # decoding failure rates
+            ax.plot(phys_err, i[5], linewidth = 2, label = i[0] + 'non DF LE') # non decoding failure logical error rates 
 
     if grid == True:
         ax.grid(which="major", alpha=0.6)
